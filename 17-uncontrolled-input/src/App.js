cf593f7cdabe9dc3c72bb04cb4441c91742c6efa
BohdanKutia777 <87551218+BohdanKutia777@users.noreply.github.com>
2023-03-05 14:13:58 +0200

    done project 17-uncontrolled-input

--- 17-uncontrolled-input/src/App.js
+++ 17-uncontrolled-input/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
-
+import Login from './components/Login';
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
 }
 export default App;
