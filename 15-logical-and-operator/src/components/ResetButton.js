7e7645d94060b64636cc463d20dd7867b29f1d76
BohdanKutia777 <87551218+BohdanKutia777@users.noreply.github.com>
2023-03-05 13:10:57 +0200

    add components ResetButton

---
+++ 15-logical-and-operator/src/components/ResetButton.js
@@ -0,0 +1,11 @@
+function ResetButton() {
+  return (
+    <div>
+      <button style={buttonStyle} onClick={resetCount}>
+        Reset
+      </button>
+    </div>
+  );
+}
+
+export default ResetButton;
