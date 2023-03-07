32156fb34d6d413ec8a9b5244f6270904fa8ce6f
BohdanKutia777 <87551218+BohdanKutia777@users.noreply.github.com>
2023-03-07 15:45:37 +0200

    done project 23-css-modules

--- 23-css-modules/src/App.js
+++ 23-css-modules/src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
+import Info from './components/Info';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Info />
+      <div className="info">
+        <h1>App component</h1>
+        <button className="my-button">App component button</button>
+        <h2>Heading in the Info component</h2>
+      </div>
+    </div>
+  );
 }
 export default App;
