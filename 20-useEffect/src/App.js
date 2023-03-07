21f1013ebe6823719bc6e154e8973ce73e131bf4
BohdanKutia777 <87551218+BohdanKutia777@users.noreply.github.com>
2023-03-07 12:33:56 +0200

    done project 20-useEffect

--- 20-useEffect/src/App.js
+++ 20-useEffect/src/App.js
@@ -1,6 +1,19 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  const [todo, setTodo] = useState(null);
+
+  useEffect(() => {
+    console.log('Callback in useEffect called');
+    fetch('https://jsonplaceholder.typicode.com/todos/3')
+      .then((response) => response.json())
+      .then((json) => setTodo(json));
+  }, []);
+
+  console.log('App render');
+  console.log(todo);
+
+  return <div className="App">{todo && <h1>{todo.title}</h1>}</div>;
 }
 export default App;
